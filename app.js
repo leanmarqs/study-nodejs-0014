9d1605004850ffb1b12261def8f8258b2e8ecc59
leanmarqs <leanmarqs@gmail.com>
2025-10-05 11:46:00 -0300

    feat: cors installation.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
 
 const app = express()
 const prisma = new PrismaClient() 
 
-
+app.use(cors())
 app.use(express.json())
+
 
  
 app.get('/', (req, res) => {
