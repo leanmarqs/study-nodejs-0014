6c0d5b7afcc9bb89aa87ea38d32b6f2ea266960b
leanmarqs <leanmarqs@gmail.com>
2025-10-05 10:07:45 -0300

    feat: Prisma mysql database connection.

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient() 
 
+
+app.use(express.json())
+
+ 
 app.get('/', (req, res) => {
   res.send('Hello Weirdo!')
 
+})
+
+app.post('/users', async (req, res) => {
+  const { name, email } = req.body      
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true
+      }
+    })
+
+    res.status(201).json({
+        msg: 'User created successfully',
+        user: user
+    })
+
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create user' })
+
+  }
+})
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            email: true
+        }
+    })
+    res.json(users)
+
+    } catch (error) {   
+    res.status(500).json({ error: 'Failed to retrieve users' })
+
+  }
 })
 
 app.listen(3000, () => {
